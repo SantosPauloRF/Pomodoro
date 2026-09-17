098f23c95b4f43166e4b32b7871cc704ca7bd7ff
Paulo Faria <santos.paulo.rf@gmail.com>
2026-09-17 17:30:05 -0300

    Corrigindo bug

---
+++ src/OverlayApp.tsx
@@ -0,0 +1,14 @@
+import { OverlayPanel } from "./components/OverlayPanel";
+import { fecharOverlayNativo, motivoDoOverlay } from "./overlay/tauriOverlay";
+
+export default function OverlayApp() {
+  const motivo = motivoDoOverlay();
+  return (
+    <OverlayPanel
+      motivo={motivo}
+      onDispensar={() => {
+        void fecharOverlayNativo();
+      }}
+    />
+  );
+}
--- src/overlay-main.tsx
+++ src/overlay-main.tsx
@@ -1,20 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { OverlayPanel } from "./components/OverlayPanel";
-import { fecharOverlayNativo, motivoDoOverlay } from "./overlay/tauriOverlay";
+import OverlayApp from "./OverlayApp";
 import "./index.css";
-
-function OverlayApp() {
-  const motivo = motivoDoOverlay();
-  return (
-    <OverlayPanel
-      motivo={motivo}
-      onDispensar={() => {
-        void fecharOverlayNativo();
-      }}
-    />
-  );
-}
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
